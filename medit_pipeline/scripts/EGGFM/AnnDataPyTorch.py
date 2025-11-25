fix variance

medit_pipeline/scripts/EGGFM/AnnDataPyTorch.py
@@ -14,7 +14,7 @@ class AnnDataExpressionDataset(Dataset):
     """
 
     def __init__(self, ad: "sc.AnnData", float_dtype: np.dtype = np.float32):
-        X = ad.X
+        X = ad.obsm["X_pca"]
         if sparse.issparse(X):
             X = X.toarray()
         X = np.asarray(X, dtype=float_dtype)

medit_pipeline/scripts/EGGFM/energy_model.py
@@ -23,7 +23,7 @@ class EnergyMLP(nn.Module):
             activation = nn.Softplus()
 
         layers = []
-        in_dim = n_genes
+        in_dim = 50
         for h in hidden_dims:
             layers.append(nn.Linear(in_dim, h))
             layers.append(activation)