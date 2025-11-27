cb920341d713e15f6d74572902cc0c325c500614
pvd232 <50126818+pvd232@users.noreply.github.com>
2025-11-26 22:46:38 -0500

    debug stments

--- medit_pipeline/scripts/EGGFM/engine.py
+++ medit_pipeline/scripts/EGGFM/engine.py
@@ -100,6 +100,7 @@ class EGGFMDiffusionEngine:
         # metric strategy
         metric = self._get_metric(metric_mode)
         print("[EGGFMDiffusion Engine] metric", metric)
+        return
         state = metric.prepare(
             ad_prep=ad_prep,
             energy_model=self.energy_model,
